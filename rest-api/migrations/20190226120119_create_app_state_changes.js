c0160212b4b18c2687a03f323c8fbd841d7b9987
oleinic4 <oleinic4@msu.edu>
2019-02-27 17:57:20 -0500

    this is not working now but ill fix it

--- rest-api/migrations/20190226120119_create_app_state_changes.js
+++ rest-api/migrations/20190226120119_create_app_state_changes.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.raw('create extension if not exists "uuid-ossp"'),
     knex.schema.createTable('app_state_changes', table => {
-      table.uuid('app_name').notNullable();
+      table.uuid('application_id').notNullable();
       table.foreign('app_name').references('id').inTable('applications');
       table.string('event_name');
 	    table.uuid('vehicle_id').notNullable();
