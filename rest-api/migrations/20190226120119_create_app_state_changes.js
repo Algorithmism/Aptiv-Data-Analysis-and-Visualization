ac1642131c4b3ef22f3339a83aa97ea5ac7b0409
oleinic4 <oleinic4@msu.edu>
2019-02-27 18:43:56 -0500

    added id column that was missing

--- rest-api/migrations/20190226120119_create_app_state_changes.js
+++ rest-api/migrations/20190226120119_create_app_state_changes.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.raw('create extension if not exists "uuid-ossp"'),
     knex.schema.createTable('app_state_changes', table => {
+      table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
       table.uuid('application_id').notNullable();
       table.foreign('app_name').references('id').inTable('applications');
       table.string('event_name');
