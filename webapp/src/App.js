8542ad2a5f76d6d4df102a9a3f6c823776dd086d
kashefme <kashefme@msu.edu>
2019-02-27 23:24:40 -0500

    Added collapsible list component - no CSS yet

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import SkeletonVList from './components/SkeletonVList.js';
+//import SkeletonVList from './components/SkeletonVList.js';
+import Collap from './components/collapslist';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="apper">
-        <SkeletonVList />
+        <Collap />
       </div>
     );
   }
---
+++ webapp/src/components/collapslist.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Collapsible from 'react-collapsible';
+ 
+class Collap extends Component {
+ 
+  render(){
+    return(
+ 
+      <Collapsible trigger="Start here">
+        <p>This is the collapsible content. It can be any element or React component you like.</p>
+        <p>It can even be another Collapsible component. Check out the next section!</p>
+      </Collapsible>
+ 
+    );
+  }
+ 
+}
+ 
+export default Collap;
