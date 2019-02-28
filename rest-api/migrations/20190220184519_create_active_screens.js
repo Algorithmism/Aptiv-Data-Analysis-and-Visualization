e72fde8a69cce7404d623a9c6fe97eca08123011
oleinic4 <oleinic4@msu.edu>
2019-02-27 19:19:18 -0500

    migrations work now, all tables but ignitions now have migrations

--- rest-api/migrations/20190220184519_create_active_screens.js
+++ rest-api/migrations/20190220184519_create_active_screens.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('active_screens', table => {
       table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
       table.uuid('application_id').notNullable();
-      table.foreign('app_name').references('id').inTable('applications');
+      table.foreign('application_id').references('id').inTable('applications');
       table.string('screen_name');
 	    table.uuid('vehicle_id').notNullable();
       table.foreign('vehicle_id').references('id').inTable('vehicles');
