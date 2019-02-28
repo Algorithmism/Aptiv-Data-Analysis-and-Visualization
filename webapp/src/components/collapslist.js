c624006577034a970db09a772cd68a0652abec57
kashefme <kashefme@msu.edu>
2019-02-27 23:45:21 -0500

    testing out embdedd drop down lists stacked ontop of eachother

--- webapp/src/components/collapslist.js
+++ webapp/src/components/collapslist.js
@@ -10,6 +10,10 @@ class Collap extends Component {
         <Collapsible trigger="Start here">
             <p>This is the collapsible content. It can be any element or React component you like.</p>
             <p>It can even be another Collapsible component. Check out the next section!</p>
+            <Collapsible trigger="oh shit">
+                <p>This is the collapsible content. It can be any element or React component you like.</p>
+                <p>It can even be another Collapsible component. Check out the next section!</p>
+            </Collapsible>
         </Collapsible>
  
     );
