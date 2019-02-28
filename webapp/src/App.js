cffd3f2f4aeba5e3a8545fbfe93042d9da2e2d3f
kashefme <kashefme@msu.edu>
2019-02-28 12:06:22 -0500

    added search compenents as well as new UI to seperate infomation better, this could potentially be the final high level UI

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 //import SkeletonVList from './components/SkeletonVList.js';
 //import Collap from './components/collapslist';
-import AccList from './components/dynamicdropdown';
+//import AccList from './components/CollapsibleComp/dynamicdropdown';
+import SearchApp from './components/SearchComp/index.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="apper">
-        <AccList />
+        <SearchApp />
       </div>
     );
   }
