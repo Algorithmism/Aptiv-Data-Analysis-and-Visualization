3bb6c021a4f551aa8d2aa6cfd510304db320ebbd
oleinic4 <oleinic4@msu.edu>
2019-03-19 00:04:46 -0400

    first attempt at summary timeline table

--- webapp/src/components/GoogleComp/Tabler.js
+++ webapp/src/components/GoogleComp/Tabler.js
@@ -7,6 +7,15 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+const axios = require('axios')
+
+const getData = async () => {
+  try {
+    return await axios.get('http://localhost:8081/summary_timeline')
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 const styles = theme => ({
   root: {
@@ -25,13 +34,7 @@ function createData(name, calories, fat, carbs, protein) {
   return { id, name, calories, fat, carbs, protein };
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+const rows = getData;
 
 function SimpleTable(props) {
   const { classes } = props;
