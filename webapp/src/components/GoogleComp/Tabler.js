8d6c2acbdb1e5808da3b60826a83619afb91cdb5
Kevin You <youkevin@msu.edu>
2019-03-19 20:54:46 -0400

    Update Tabler.js
    got the axios working. but needs organization

--- webapp/src/components/GoogleComp/Tabler.js
+++ webapp/src/components/GoogleComp/Tabler.js
@@ -7,13 +7,27 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-const axios = require('axios')
+import axios from 'axios';
 
-const getData = async () => {
-  try {
-    return await axios.get('http://localhost:8081/summary_timeline')
-  } catch (error) {
-    console.error(error)
+class VehicleList extends React.Component{
+  state = {
+    vehicles: [],
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:8088/vehicles').then(response => {
+      console.log(response);
+      this.setState({vehicles: response.data})
+    });
+  }
+  render() {
+    console.log(this.state.vehicles);
+    //return this.state.vehicles;
+    return (
+      <ul>
+        {this.state.vehicles.map(vehicle => <li>{vehicle.name}</li>)}
+      </ul>
+    )
   }
 }
 
@@ -33,13 +47,25 @@ function createData(name, calories, fat, carbs, protein) {
   id += 1;
   return { id, name, calories, fat, carbs, protein };
 }
-
-const rows = getData;
-
+/*
+const rows = VehicleList;
+console.log(rows);
+*/
+/*
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+*/
 function SimpleTable(props) {
   const { classes } = props;
 
   return (
+    <VehicleList />
+    /*
     <Paper className={classes.root}>
       <Table className={classes.table}>
         <TableHead>
@@ -57,7 +83,7 @@ function SimpleTable(props) {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.name}</TableCell>
               <TableCell align="right">{row.fat}</TableCell>
               <TableCell align="right">{row.carbs}</TableCell>
               <TableCell align="right">{row.protein}</TableCell>
@@ -66,6 +92,7 @@ function SimpleTable(props) {
         </TableBody>
       </Table>
     </Paper>
+    */
   );
 }
 
