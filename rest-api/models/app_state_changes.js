d8ca4e2d646e765f6cf8e1d9c7cb30336867066b
Abram Stamper <9876126+abramjstamper@users.noreply.github.com>
2019-03-25 16:22:31 -0400

    Update app_state_changes.js

--- rest-api/models/app_state_changes.js
+++ rest-api/models/app_state_changes.js
@@ -5,6 +5,19 @@ class App_State_Changes extends Model {
   static get tableName() {
       return 'app_state_changes';
   }
+  
+   static get relationMappings() {
+    return {
+      application: {
+        relation: BaseModel.BelongsToOneRelation,
+        modelClass: `${__dirname}/applications`,
+        join: {
+          from: 'app_state_changes.application_id',
+          to: 'applications.id',
+        }
+      }
+    };
+  }
 }
 
 exports.App_State_Changes = App_State_Changes;
