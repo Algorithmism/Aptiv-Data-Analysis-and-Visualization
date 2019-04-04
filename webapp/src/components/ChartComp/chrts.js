a849f49aaa925d8c8f9035c201628cf47d5e5a63
Abram Stamper <abram.stamper@aptiv.com>
2019-04-04 15:10:43 -0400

    Update chrts.js
    resolved bugs?

--- webapp/src/components/ChartComp/chrts.js
+++ webapp/src/components/ChartComp/chrts.js
@@ -7,38 +7,11 @@ var idd = [];
 var uses = [];
 var eventer;
 
-
-
-
 class Charter extends React.Component {
 
-
-  state = {
-    events: [],
-  };
-
-  
-  componentDidMount() {
-    axios.get('http://localhost:8081/app_usages').then(response => {
-      console.log(response);
-      const events = response.data;
-      this.setState({events: response.data})
-  })
-    
-    //this.state.events.map(event => {
-      //uses.push(event.uses);
-      //idd.push(event.vehicle_name +  " -- " + event.application_name);
-    //})
-    
-
-  } 
-  constructor(props) {
-
-
-  
+ constructor(props) { 
       super(props);
-
-      
+   
       this.state = {
         options: {
           chart: {
@@ -52,18 +25,27 @@ class Charter extends React.Component {
           name: 'series-1',
           data: uses
         }],
-
-
+        events: []
       }
     }
+  
+  async componentDidMount() {
+    const response = await axios.get('http://localhost:8081/app_usages');
+    const events = response.data;
+    this.setState({events: response.data},
+    this.state.events.map(event => {
+      uses.push(event.uses);
+      idd.push(event.vehicle_name +  " -- " + event.application_name);
+    }));
+  } 
+  
     render() {
       return (
         <div className="bill">
             <Chart options={this.state.options} series={this.state.series} type="bar" width={1000} height={320} />
         </div>
-      )
+      );
     }
   }
 
-
-  export default Charter;
+export default Charter;
