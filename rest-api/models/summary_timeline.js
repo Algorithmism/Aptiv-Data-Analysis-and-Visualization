7c1087f9a42a41630253311e75c2d9905269e0d0
oleinic4 <oleinic4@msu.edu>
2019-04-17 08:14:33 -0400

    added eager functionality to model

--- rest-api/models/summary_timeline.js
+++ rest-api/models/summary_timeline.js
@@ -5,6 +5,34 @@ class Summary_Timeline extends Model {
   static get tableName() {
       return 'summary_timeline';
   }
+
+  static get relationMappings() {
+    return {
+      application: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: `${__dirname}/applications`,
+        join: {
+          from: 'summary_timeline.application_id',
+          to: 'applications.id',
+        }
+      }
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      vehicle: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: `${__dirname}/vehicles`,
+        join: {
+          from: 'summary_timeline.vehicle_id',
+          to: 'vehicles.id',
+        }
+      }
+    };
+  }
+
+
 }
 
 exports.Summary_Timeline = Summary_Timeline;
